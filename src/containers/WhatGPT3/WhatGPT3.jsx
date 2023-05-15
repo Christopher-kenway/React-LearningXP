7dadfbc1cad51760f6619313a81093054ef13aeb
Christopher-kenway <maero1eguwe@gmail.com>
2023-05-15 23:01:43 +0100

    Initial commit

---
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './WhatGPT3.css';
+import { Feature } from '../../components';
+
+
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section-margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+       <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient-text'>The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+    
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+         <Feature />
+         <Feature />
+         <Feature />
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
