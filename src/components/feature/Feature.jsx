2b1f1ed6a75c175d59d8f4675f458a6dd18bdf2a
Christopher-kenway <maero1eguwe@gmail.com>
2023-05-16 01:19:55 +0100

    new changes

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,8 +3,12 @@ import './feature.css';
 
 const Feature = () => {
   return (
-    <div>
-      Feature
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__feature-container__feature-title'>
+        <h1>title</h1>
+      </div>
+      <div className='gpt3__features-coontainer'></div>
+      
     </div>
   );
 };
--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -2,26 +2,23 @@ import React from 'react';
 import './WhatGPT3.css';
 import { Feature } from '../../components';
 
-
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section-margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-       <Feature />
+        <Feature />
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient__text'>The possibilities are beyond your imagination
-        </h1>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
-    
       </div>
       <div className='gpt3__whatgpt3-container'>
-         <Feature />
-         <Feature />
-         <Feature />
+        <Feature />
+        <Feature />
+        <Feature />
       </div>
     </div>
-  )
+  );
 }
 
-export default WhatGPT3
+export default WhatGPT3;
